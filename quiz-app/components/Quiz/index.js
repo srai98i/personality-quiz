c8faf10d7ed989d544bf28d1ac064a106e1f8361
srai98i <simran_rai982@outlook.com>
2022-07-04 18:09:56 +0100

    quiz logic plan

--- quiz-app/components/Quiz/index.js
+++ quiz-app/components/Quiz/index.js
@@ -27,5 +27,34 @@ PLAN
 - decide on the state management strategy
   > context
   > Redux
-  
+ */
+
+/* 
+PLAN FOR QUIZ LOGIC
+- make lib folder with question array > renders on page 
+- answers array in Quiz, use a handleAnswer function to push values into array (...spread)
+
+- array to add answer values to 
+  > sort array to find most common value 
+  > assign a stage of a heros journey to each value
+  > return that stage when submit is clicked 
+
+handleAnswer functions
+ - onclick prop for each button 
+ - 5 functions to handle each button
+ - each button has own array of answers
+ - buttons add something to the array
+ - submit button handles calculating result 
+
+Submit button
+- handleSubmit prop for submit button 
+- when submit is clicked, result rendered under submit button 
+- when submit is clicked, state is changed to show the correct result // result, setResult 
+- initial state is empty string
+  => length of each array is compared 
+  => longest array is the final answer
+  => show updated state (use switch statement) => hero's journey stage
+
+
+
  */
