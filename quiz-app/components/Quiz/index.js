9e2f2979ee97a92e515d29045d7c1171c810f542
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-10 14:29:54 +0100

    Fixed Submit button

--- quiz-app/components/Quiz/index.js
+++ quiz-app/components/Quiz/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Question from "../Question";
-import SubmitButton from "../SubmitButton";
 import questionsArray from "../../lib/quizData";
 
 // key needs to be on the looped elements - the parent element
@@ -25,69 +24,24 @@ export default function Quiz() {
     console.log(sortedArray);
   };
 
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(answers);
+    };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <ol>
         {questionsArray.map((questionText, index) => (
           <li key={index.toString()}>
             <Question
               question={questionText}
-              onChange={(buttonType) =>
-                handleChange(index, buttonType)
-              }></Question>
+              onChange={(buttonType) => handleChange(index, buttonType)}
+            ></Question>
           </li>
         ))}
       </ol>
-      <SubmitButton getQuizResult={getQuizResult} answers={answers} />
+      <input type="submit" />
     </form>
   );
 }
-
-/* 
-- Radio buttons
-- One big state or smaller states with useReducer;
-- UseReducer is best used when we have a state within state -
-*/
-
-/*
-PLAN
-- for answer and submit functionality 
-  > record user submission for each question
-  > store each score 
-  > submit button calculates an overall score 
-  > translates to stage in hero journey 
-- decide on the state management strategy
-  > context
-  > Redux
- */
-
-/* 
-PLAN FOR QUIZ LOGIC
-- make lib folder with question array > renders on page ✅ 
-- answers array in Quiz, use a handleAnswer function to push values into array (...spread)
-
-- array to add answer values to 
-  > sort array to find most common value 
-  > assign a stage of a heros journey to each value
-  > return that stage when submit is clicked 
-
-handleAnswer functions
- - onclick prop for each button 
- - 5 functions to handle each button
- - each button has own array of answers
- - buttons add something to the array
- - submit button handles calculating result 
-
-Submit button
-- handleSubmit prop for submit button 
-- when submit is clicked, result rendered under submit button 
-- when submit is clicked, state is changed to show the correct result // result, setResult 
-- initial state is empty string
-  => length of each array is compared 
-  => longest array is the final answer
-  => show updated state (use switch statement) => hero's journey stage
-
-
-
-  -- Inputs with radios with labels associate with labels
- */
