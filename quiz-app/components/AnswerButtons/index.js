f10f322515acd5113a65bde719c1c4ce8623221e
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-13 18:14:06 +0100

    Configured Tailwind and started working on NavBar

--- quiz-app/components/AnswerButtons/index.js
+++ quiz-app/components/AnswerButtons/index.js
@@ -1,2 +1,1 @@
 export {default} from './AnswerButtons.jsx';
-// export {default as Comp1} from './Comp1.jsx';
---
+++ quiz-app/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Journey Types</li>
+                <li>Login</li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
