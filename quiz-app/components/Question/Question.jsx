c5fa519f62909385ed21e151c63244d5041bdfe2
srai98i <simran_rai982@outlook.com>
2022-07-17 14:36:35 +0100

    style: work on styling question component so size is responsive

--- quiz-app/components/Question/Question.jsx
+++ quiz-app/components/Question/Question.jsx
@@ -2,12 +2,12 @@ import AnswerButtons from "../AnswerButtons";
 
 export default function Question({ question, onChange }) {
   return (
-    <section className="flex space-x-4 justify-between h-4 border-b-[1rem] bg-[#180520] [#E1A774]">
-      <label className="flex justify-self-start" htmlFor={question}>
+    <section className="flex shrink-10 relative space-x-4 left-[20vw] right-[20vw] justify-between w-[60%] h-16 border-b-[0.1rem] border-b-[#E1A774] bg-[#180520] text-[#E1A774] text-[75%] p-[1%] ">
+      <label className="flex shrink-10 justify-self-start" htmlFor={question}>
         {question} {"\n"}
       </label>
       <AnswerButtons
-        className="flex justify-self-end"
+        className="flex shrink-10 justify-self-end"
         question={question}
         onChange={onChange}
       />
