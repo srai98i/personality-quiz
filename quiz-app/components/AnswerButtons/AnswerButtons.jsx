955fc62e6a4a92ce2460b8c759425a64acbf6a48
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-17 15:38:40 +0100

    style: resize question fonts and work on radio buttons

--- quiz-app/components/AnswerButtons/AnswerButtons.jsx
+++ quiz-app/components/AnswerButtons/AnswerButtons.jsx
@@ -5,7 +5,8 @@ export default function AnswerButtons({ question, onChange }) {
     <>
       {answerOptions.map((buttonType) => (
         <span key={question + buttonType}>
-          <input className="flex self-center"
+          <input
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
             type="radio"
             name={question}
             key={question + buttonType}
@@ -13,7 +14,12 @@ export default function AnswerButtons({ question, onChange }) {
             value={buttonType}
             onClick={() => onChange(buttonType)}
           />
-          <label className="flex flex-col self-center" htmlFor={question + buttonType}>{buttonType}</label>
+          <label
+            className="flex flex-col self-center"
+            htmlFor={question + buttonType}
+          >
+            {buttonType}
+          </label>
         </span>
       ))}
     </>
