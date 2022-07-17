0313d53c46e2eb5fdb31ccd23f5ce0ce4b29a69e
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-17 10:45:35 +0100

    style: Style the navigation bar with flex in Tailwind

--- quiz-app/components/NavBar/NavBar.jsx
+++ quiz-app/components/NavBar/NavBar.jsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 
 export default function NavBar() {
     return (
-        <nav className="flex justify-end gap-x-80 gap-y-160 ">
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            <ul>
-                <li>Journey Types</li>
-                <li>Login</li>
-   
-            </ul>
-        </nav>
-    )
+      <nav className="flex justify-between gap-x-160 gap-y-160 ">
+        <Image className = "flex justify-self-start" src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <ul className="flex justify-self-end space-x-4 ">
+          <li>Journey Types</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    );
 }
 
