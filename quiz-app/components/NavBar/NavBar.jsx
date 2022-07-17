f7319467323102c549c31ee93c29c5a99fa05709
AfamOkoh <afamokoh@gmail.com>
2022-07-17 11:34:16 +0100

    style: work on importing font and change height of navbar

--- quiz-app/components/NavBar/NavBar.jsx
+++ quiz-app/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function NavBar() {
     return (
-      <nav className="flex justify-between bg-[#271E2B] w-full h-20">
+      <nav className="flex justify-between bg-[#271E2B] w-full h-[13%]">
         <Image className = "flex justify-self-start" src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         <ul className="flex justify-self-end space-x-4 text-[#F4EFEF]">
           <li>Journey Types</li>
