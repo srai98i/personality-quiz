58708240e0b8e3c8af50fe9b42fa098f24fa1e3a
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-18 11:52:34 +0100

    resized the hr tag and refactoring radio buttons

--- quiz-app/components/AnswerButtons/AnswerButtons.jsx
+++ quiz-app/components/AnswerButtons/AnswerButtons.jsx
@@ -4,9 +4,9 @@ export default function AnswerButtons({ question, onChange }) {
   return (
     <>
       {answerOptions.map((buttonType) => (
-        <span key={question + buttonType}>
+        <span key={question + buttonType} className="max-w-sm border-yellow">
           <input
-            className="appearance-none w-8 h-8 checked:bg-[#E1A774] rounded-full box-border border-solid border-2 border-[#E1A774] "
+            className="appearance-none w-8 h-8 checked:bg-[#E1A774] rounded-full box-border border-solid border-2 border-[#E1A774] justify-end"
             type="radio"
             name={question}
             key={question + buttonType}
--- quiz-app/components/Question/Question.jsx
+++ quiz-app/components/Question/Question.jsx
@@ -2,24 +2,28 @@ import AnswerButtons from "../AnswerButtons";
 
 export default function Question({ question, onChange }) {
   return (
-    <section className="flex flex-col box-border">
-      <div className="flex relative  space-x-4 divide-y-10 left-[20vw] right-[20vw] justify-between w-[60%] h-16 bg-[#180520] text-[#E1A774] p-[8%] min-h-full text-[1rem] scale-100">
+    <>
+      <section className="flex box-border place-content-evenly relative  space-x-4 divide-y-10 left-[20vw] right-[20vw] justify-between w-[60%] h-16 bg-[#180520] text-[#E1A774] p-[8%] min-h-full text-[1rem] scale-100">
+        {/* <div className="flex relative  space-x-4 divide-y-10 left-[20vw] right-[20vw] justify-between w-[60%] h-16 bg-[#180520] text-[#E1A774] p-[8%] min-h-full text-[1rem] scale-100"> */}
         {/* <div className="flex self-start object-scale-down"> */}
-          <label className="flex  justify-self-start" htmlFor={question}>
-            {question} {"\n"}
-          </label>
+        <label className="flex justify-start" htmlFor={question}>
+          {question} {"\n"}
+        </label>
         {/* </div> */}
-
         <AnswerButtons
           className="flex flex-row "
           question={question}
           onChange={onChange}
         />
-      </div>
-      <p></p> {/*for spacing*/}
+        {/* </div> */}
+        <p></p> {/*for spacing*/}
+        {/* <div className="flex flex-col h-full w-full">
+        <hr className="bg-[#E1A774] w-[60%] border-[#E1A774] h-1 self-center justify-center " />
+      </div> */}
+      </section>
       <div className="flex flex-col h-full w-full">
         <hr className="bg-[#E1A774] w-[60%] border-[#E1A774] h-1 self-center justify-center " />
       </div>
-    </section>
+    </>
   );
 }
