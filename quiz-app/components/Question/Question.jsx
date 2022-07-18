af9b0ed41e455ab147121aa2a9bc5d29f5a2f841
srai98i <simran_rai982@outlook.com>
2022-07-18 12:23:59 +0100

    style: changing positioning of question

--- quiz-app/components/Question/Question.jsx
+++ quiz-app/components/Question/Question.jsx
@@ -2,10 +2,10 @@ import AnswerButtons from "../AnswerButtons";
 
 export default function Question({ question, onChange }) {
   return (
-    <section className="flex relative justify-items-center w-screen h-[5rem] bg-[#180520] text-[#E1A774]  border-b-[#E1A774] box-border border-[0.5rem] text-[0.8rem] scale-100">
+    <section className="flex relative justify-items-center w-[150%] h-[5rem] bg-[#180520] text-[#E1A774]  border-b-[#E1A774] box-border border-b-[0.25rem] text-[0.8rem] scale-100 p-[0.5rem] m-[0.5rem]">
       
         {/* <div className="flex self-start object-scale-down"> */}
-          <label className="flex  place-self-center text-[0.8em] w-[30%]" htmlFor={question}>
+          <label className="flex  place-self-center text-[0.5rem] w-[30%] h-[30%] m-[0.5rem]" htmlFor={question}>
             {question} {"\n"}
           </label>
         {/* </div> */}
