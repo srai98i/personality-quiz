f933c2447baa481d929ad20aa5e21c4ef5ce7281
srai98i <simran_rai982@outlook.com>
2022-07-18 10:40:44 +0100

    style: attempting to fix overflow

--- quiz-app/components/Question/Question.jsx
+++ quiz-app/components/Question/Question.jsx
@@ -2,19 +2,20 @@ import AnswerButtons from "../AnswerButtons";
 
 export default function Question({ question, onChange }) {
   return (
-    <section className="flex shrink-10 relative space-x-4 left-[20vw] right-[20vw] justify-between w-[60%] h-16 border-b-[0.1rem] border-b-[#E1A774] bg-[#180520] text-[#E1A774] text-[3vh] p-[8%]  ">
-      <div className="flex self-start w-[100%]">
+    <section className="flex shrink-10 relative space-x-4 left-[20vw] right-[20vw] justify-between w-[60%] h-16 bg-[#180520] text-[#E1A774] p-[8%] min-h-full text-[.5rem] scale-100">
+      <div className="flex self-start object-scale-down">
       <label className="flex  justify-self-start" htmlFor={question}>
         {question} {"\n"}
       </label>
       </div>
       
       <AnswerButtons
-        className="flex  flex-row"
+        className="flex flex-row  object-scale-down"
         question={question}
         onChange={onChange}
       />
       <p></p> {/*for spacing*/}
+      <hr className=" border-b-[#E1A774] w-[50%]"/>
     </section>
   );
 }
