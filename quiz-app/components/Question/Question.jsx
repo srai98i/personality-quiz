dc30e9eda86ad4c69e10e9e756cff02d520c1099
srai98i <simran_rai982@outlook.com>
2022-07-18 13:57:13 +0100

    style: remove borders from section and div

--- quiz-app/components/Question/Question.jsx
+++ quiz-app/components/Question/Question.jsx
@@ -2,9 +2,9 @@ import AnswerButtons from "../AnswerButtons";
 
 export default function Question({ question, onChange }) {
   return (
-    <section className="flex relative justify-items-center w-[200%] h-[5rem] bg-[#180520] text-[#E1A774]  border-[#E1A774] box-border border-[0.25rem] text-[0.8rem] scale-100 p-[0.5rem] m-[0.5rem]">
+    <section className="flex relative justify-items-center w-[200%] h-[5rem] bg-[#180520] text-[#E1A774]  border-b-[#E1A774] box-border border-b-[0.25rem] text-[0.8rem] scale-100 p-[0.5rem] m-[0.5rem]">
       
-        <div className="flex text-[.75em] min-h-full w-[30%] h-[4em] border-white box-border border-[0.25rem]">
+        <div className="flex text-[.75em] min-h-full w-[30%] h-[4em]">
           <label  htmlFor={question}>
             {question} {"\n"}
           </label>
