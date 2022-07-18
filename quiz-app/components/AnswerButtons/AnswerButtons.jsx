4a1a1f40e2089f39950714a0c47ad5b6b616d9b5
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-18 14:40:41 +0100

    style: refactor Answer Button components

--- quiz-app/components/AnswerButtons/AnswerButtons.jsx
+++ quiz-app/components/AnswerButtons/AnswerButtons.jsx
@@ -4,9 +4,12 @@ export default function AnswerButtons({ question, onChange }) {
   return (
     <>
       {answerOptions.map((buttonType) => (
-        <span className="w-[60%] border-[#E1A774] box-border border-[0.1rem]" key={question + buttonType}>
+        <div
+          className=" flex flex-col items-center w-[10%] border-[#E1A774] box-border border-[0.1rem]"
+          key={question + buttonType}
+        >
           <input
-            className="appearance-none w-[1em] h-[1em]  checked:bg-[#E1A774] rounded-full box-border border-solid border-2 border-[#E1A774] "
+            className=" m-[0.5em] appearance-none w-[3em] h-[3em] checked:bg-[#E1A774] rounded-full box-border border-solid border-2 border-[#E1A774] "
             type="radio"
             name={question}
             key={question + buttonType}
@@ -15,12 +18,12 @@ export default function AnswerButtons({ question, onChange }) {
             onClick={() => onChange(buttonType)}
           />
           <label
-            className="flex flex-col  w-[90%] h-[1rem] self-center"
+            className="flex flex-col items-center w-[90%] h-[1rem] border-green-500 box-border border-[0.1rem]"
             htmlFor={question + buttonType}
           >
             {buttonType}
           </label>
-        </span>
+        </div>
       ))}
     </>
   );
